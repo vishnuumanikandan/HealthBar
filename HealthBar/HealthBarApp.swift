3ec66e761c9e310c3e7d279a76fb714345969547
Vishnu Manikandan <vishnunatty@gmail.com>
2026-01-12 20:52:39 -0800

    Initial Commit

---
+++ HealthBar/HealthBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthBarApp.swift
+//  HealthBar
+//
+//  Created by Vishnu Nathan on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthBarTests/HealthBarTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthBarTests.swift
+//  HealthBarTests
+//
+//  Created by Vishnu Nathan on 1/12/26.
+//
+
+import Testing
+
+struct HealthBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
